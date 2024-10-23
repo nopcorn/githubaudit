c8be19cfd3ea1a891e2bec5c19bc0575dd4a7ef8
nopcorn <143365389+nopcorn@users.noreply.github.com>
2024-10-23 12:12:15 -0400

    breaking change in later versions of requests

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "requests",
+        "requests==2.32.3",
         "jinja2",
     ],
     entry_points={
@@ -31,11 +31,12 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Security"
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 )
