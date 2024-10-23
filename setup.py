c9dab20c05e127a605310036ce32c5eba1a9f7b0
nopcorn <143365389+nopcorn@users.noreply.github.com>
2024-10-23 11:46:47 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="githubaudit",
+    version="0.1.0",
+    author="nopcorn",
+    author_email="",
+    description="Uses a Github PAT to assess the security configuration of repositories and provides a report",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nopcorn/githubaudit",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "jinja2",
+    ],
+    entry_points={
+        "console_scripts": [
+            "githubaudit=githubaudit.cli:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Security"
+    ],
+    python_requires=">=3.6",
+)
